340a196edb1a6e846e9d77baf28e687bbe195c95
azon <32076614+AZ0N@users.noreply.github.com>
2023-09-06 14:33:21 +0200

    add grayscale and threshold

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "cbmp.h"
 
+const int THRESHOLD = 100;
+
 // Function to invert pixels of an image (negative)
 void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
 {
@@ -17,9 +19,48 @@ void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsi
   }
 }
 
+void grayscale(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      unsigned char average = (input_image[x][y][0] + input_image[x][y][1] + input_image[x][y][2]) / 3;
+      output_image[x][y] = average;
+      // printf("0 - %d, 1 - %d, 2 - %d, average - %d\n", input_image[x][y][0], input_image[x][y][1], input_image[x][y][2], average);
+    }
+  }
+}
+
+void threshold(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      input_image[x][y] = input_image[x][y] > THRESHOLD ? 255 : 0;
+    }
+  }
+}
+
+void grayscale_to_multi_channel(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      for (int c = 0; c < BMP_CHANNELS; c++)
+      {
+        output_image[x][y][c] = input_image[x][y];
+      }
+    }
+  }
+}
+
 // Declaring the array to store the image (unsigned char = unsigned 8 bit)
 unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
+unsigned char grayscale_image[BMP_WIDTH][BMP_HEIGTH];
 
 // Main function
 int main(int argc, char **argv)
@@ -41,8 +82,14 @@ int main(int argc, char **argv)
   // Load image from file
   read_bitmap(argv[1], input_image);
 
-  // Run inversion
-  invert(input_image, output_image);
+  // Convert to grayscale
+  grayscale(input_image, grayscale_image);
+
+  // Threshold
+  threshold(grayscale_image);
+
+  // Back to multi channel 
+  grayscale_to_multi_channel(grayscale_image, output_image);
 
   // Save image to file
   write_bitmap(output_image, argv[2]);
